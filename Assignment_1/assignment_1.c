b4ddef7aef7678608719d3ae218a6d6940e42f35
RiyaAmeta <130819513+RiyaAmeta@users.noreply.github.com>
2024-12-21 20:52:41 +0530

    Update assignment_1.c

--- Assignment_1/assignment_1.c
+++ Assignment_1/assignment_1.c
@@ -1,83 +1,103 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
-void addition_subtraction(char *exp);
-int main(){
-    char exp[100];
-    int cnt =0;
-    int len = strlen(exp);
-    printf("Enter an expression: ");
-    fgets(exp, 100, stdin);
-    exp[strcspn(exp, "\n")] = '\0';
-    for(int i =0; exp[i] !='\0' ;i++){
-        if(exp[i] != ' '){
-            exp[cnt++]=exp[i];
-    }
-    }
-    exp[cnt] ='\0';
-    addition_subtraction(exp);
-}
-void addition_subtraction(char *exp){ 
+#include <ctype.h>
+void DMAS_Calculator(char *math_expression);
+int main()
+{
+    char math_expression[100];
+    int count = 0;
 
-    int len = strlen(exp);  
-    for(int i=0;i<len;i++){
-        if(exp[i]!= '*'&&exp[i]!='+'&&exp[i]!='-'&&exp[i]!='/'&&exp[i]!='0'&&exp[i]!='1'&&exp[i]!='2'&&exp[i]!='3'&&exp[i]!='4'&&exp[i]!='5'&&exp[i]!='6'&&exp[i]!='7'&&exp[i]!='8'&&exp[i]!='9'){
+    printf("Enter an mathematical expression: ");
+    fgets(math_expression, 100, stdin);
+    math_expression[strcspn(math_expression, "\n")] = '\0';
+    for (int i = 0; math_expression[i] != '\0'; i++)
+    {
+        if (math_expression[i] != ' ')
+        {
+            math_expression[count++] = math_expression[i];
+        }
+    }
+    math_expression[count] = '\0';
+    int string_length = strlen(math_expression);
+    for (int i = 0; i < string_length; i++)
+    {
+        char ch = math_expression[i];
+        if (!isdigit(ch) && ch != '+' && ch != '-' && ch != '*' && ch != '/')
+        {
             printf("Error: Invalid expression\n");
-            return;
+            return 0;
         }
     }
-    for(int i =0;i<len;i++){
-        if(exp[i]=='*'|| exp[i]=='/'){
-            int a = exp[i-1] -'0';
-            int b = exp[i+1] -'0';
+    DMAS_Calculator(math_expression);
+    return 0;
+}
+void DMAS_Calculator(char *math_expression)
+{
+    int string_length = strlen(math_expression);
+    for (int i = 0; i < string_length; i++)
+    {
+        if (math_expression[i] == '*' || math_expression[i] == '/')
+        {
+            int a = math_expression[i - 1] - '0';
+            int b = math_expression[i + 1] - '0';
             int result;
-            if(exp[i]=='*'){
-                result = a*b;
+            if (math_expression[i] == '*')
+            {
+                result = a * b;
             }
-            if(exp[i]=='/'){
-                result = a/b;
+            if (math_expression[i] == '/')
+            {
+                result = a / b;
             }
             char temp[10];
             sprintf(temp, "%d", result);
 
-            int j = i-1;
-            for(int k =0; temp[k] !='\0';j++,k++){
-                exp[j] = temp[k];
+            int j = i - 1;
+            for (int k = 0; temp[k] != '\0'; j++, k++)
+            {
+                math_expression[j] = temp[k];
             }
-            for(int k =i+2;k<len;j++,k++){
-                exp[j] = exp[k];
+            for (int k = i + 2; k < string_length; j++, k++)
+            {
+                math_expression[j] = math_expression[k];
             }
-            exp[j] = '\0';
-            len = strlen(exp);
-            i =-1;
+            math_expression[j] = '\0';
+            string_length = strlen(math_expression);
+            i = -1;
         }
     }
-        
 
-    for(int i=0;i<len;i++){
-        if(exp[i]=='+'|| exp[i]=='-'){
-            int a = exp[i-1] -'0';
-            int b = exp[i+1] -'0';
+    for (int i = 0; i < string_length; i++)
+    {
+        if (math_expression[i] == '+' || math_expression[i] == '-')
+        {
+            int a = math_expression[i - 1] - '0';
+            int b = math_expression[i + 1] - '0';
             int result;
-            if(exp[i]=='+'){
-                result = a+b;
+            if (math_expression[i] == '+')
+            {
+                result = a + b;
             }
-            if(exp[i]=='-'){
-                result = a-b;
+            if (math_expression[i] == '-')
+            {
+                result = a - b;
             }
             char temp[10];
             sprintf(temp, "%d", result);
 
-            int j = i-1;
-            for(int k =0; temp[k] !='\0';j++,k++){
-                exp[j] = temp[k];
+            int j = i - 1;
+            for (int k = 0; temp[k] != '\0'; j++, k++)
+            {
+                math_expression[j] = temp[k];
             }
-            for(int k =i+2;k<len;j++,k++){
-                exp[j] = exp[k];
+            for (int k = i + 2; k < string_length; j++, k++)
+            {
+                math_expression[j] = math_expression[k];
             }
-            exp[j] = '\0';
-            len = strlen(exp);
-            i =-1;
+            math_expression[j] = '\0';
+            string_length = strlen(math_expression);
+            i = -1;
         }
     }
-    printf("The answer for the exp is: %s\n",exp);
+    printf("The answer for the math_expression is: %s\n", math_expression);
 }
