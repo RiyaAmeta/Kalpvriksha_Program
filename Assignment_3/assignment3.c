0d74985877d16f603d88460583ec8246a1050f6e
RiyaAmeta <130819513+RiyaAmeta@users.noreply.github.com>
2025-01-08 10:51:53 +0530

    Update assignment3.c

--- Assignment_3/assignment3.c
+++ Assignment_3/assignment3.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
 #include <math.h>
-int ModularExponentiation(int base, int exponent, int modulus)
-{
-    if (modulus < 1)
-    {
-        printf("Modulus value M should be greater than 1\n");
-        return -1;
-    }
+#include<stdlib.h>
 
-    int result = 1;
+void ModularExponentiation(int base, int exponent, int modulus, int *result)
+{
+    *result = 1;
     base = base % modulus;
 
     while (exponent > 0)
     {
         if (exponent % 2 == 1)
         {
-            result = (result * base) % modulus;
+            *result = (*result * base) % modulus;
         }
 
         exponent = exponent / 2;
         base = (base * base) % modulus;
     }
-
-    return result;
 }
 
 int main()
 {
-    int B, N, M;
-
+    int B, N, M, *result;
+    
     printf("Enter base (B): ");
     scanf("%d", &B);
     printf("Enter exponent (N): ");
     scanf("%d", &N);
     printf("Enter modulus (M): ");
     scanf("%d", &M);
-
-    int result = ModularExponentiation(B, N, M);
-
-    if (result != -1)
+    if (M < 1)
     {
-        printf("Result: %d\n", result);
+        printf("Modulus value M should be greater than 1\n");
+        return 1;
+    }
+    ModularExponentiation(B, N, M, result);
+
+    if (*result != -1)
+    {
+        printf("Result: %d\n", *result);
     }
 
     return 0;
